59457ae36c3da6f95dbaae1a2fedf71ff12168d2
Вячеслав Цыкин <tsykin.vyacheslav@otr.ru>
2015-08-19 20:39:20 +0300

    Initial commit

---
+++ Backend/src/main/java/ru/moeradio/backend/MoeRadioApplication.java
@@ -0,0 +1,12 @@
+package ru.moeradio.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeRadioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoeRadioApplication.class, args);
+    }
+}
---
+++ Backend/src/main/java/ru/moeradio/backend/ServletInitializer.java
@@ -0,0 +1,13 @@
+package ru.moeradio.backend;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MoeRadioApplication.class);
+	}
+
+}
