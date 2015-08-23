bdb2443a2a1f4670bfda08a16164f10ee7276f3a
Вячеслав Цыкин <tsykin.vyacheslav@otr.ru>
2015-08-23 19:13:36 +0300

    Add ScanService class

---
+++ Backend/src/main/java/ru/moeradio/backend/service/ScanService.java
@@ -0,0 +1,18 @@
+package ru.moeradio.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * ru.moeradio.backend.service
+ * Backend
+ *
+ * @author Tsykin V.A.
+ *         tsykin.vyacheslav@otr.ru
+ * @date 23.08.15
+ */
+@Service
+public class ScanService {
+    @Autowired
+    private TrackService trackService;
+}
